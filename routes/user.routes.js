e4877c6184907f9bba15605108c6e4e0c9dc74cc
Negar Baharmand <negarbaharmand@outlook.com>
2025-04-18 20:09:39 +0200

    Add getUsers and getUser endpoints to UserController; implement JWT-based authorization middleware

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,10 +1,12 @@
 import {Router} from 'express';
+import {getUser, getUsers} from "../controllers/user.controller.js";
+import authorize from "../middlewares/auth.middleware.js";
 
 const userRoutes = Router();
 
-userRoutes.get('/', (req, res) => res.send({title: 'Get all users'}));
+userRoutes.get('/', getUsers);
 
-userRoutes.get('/:id', (req, res) => res.send({title: 'Get user details'}));
+userRoutes.get('/:id',authorize, getUser);
 
 userRoutes.post('/', (req, res) => res.send({title: 'Create new user'}));
 
